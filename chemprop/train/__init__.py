c6b6a1fe384e9ee9144393ad5c9b0dc584aef37d
Fangyin <61076726+ZJU-Fangyin@users.noreply.github.com>
2024-01-28 21:02:25 +0800

    Update __init__.py

--- chemprop/train/__init__.py
+++ chemprop/train/__init__.py
@@ -2,3 +2,4 @@ from .evaluate import evaluate, evaluate_predictions
 from .predict import predict, get_emb
 from .run_training import run_training, pre_training
 from .train import train
+from .make_predictions import make_predictions
